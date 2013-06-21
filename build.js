23c0db86016f4bb67618263d0f2678e8fd2986e7
david.bashford <dbashford@bericotechnologies.com>
2013-06-21 12:04:26 -0400

    references #221, attempt at cross-platform build

---
+++ build.js
@@ -0,0 +1,11 @@
+var path = require('path')
+  , exec = require('child_process').exec
+  , binPath = path.join(__dirname, "bin", "mimosa");
+
+exec(binPath + " build", function (error, stdout, stderr) {
+  console.log('stdout: ' + stdout);
+  console.log('stderr: ' + stderr);
+  if (error !== null) {
+    console.log('exec error: ' + error);
+  }
+})
