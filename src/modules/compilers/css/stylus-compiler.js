76467183aa9ec4383329f16c587584e24baadf1a
david.bashford <dbashford@bericotechnologies.com>
2013-12-25 07:50:52 -0500

    references #316, stylus compiler to javascript, pre-2.0 compiler work

--- src/modules/compilers/css/stylus-compiler.js
+++ src/modules/compilers/css/stylus-compiler.js
@@ -71,40 +71,7 @@ var compile = function (file, config, options, done) {
   stylusSetup.render( cb );
 };
 
-var determineBaseFiles = function(allFiles) {
-  var anImport, baseFiles, file, fullFilePath, fullImportPath, importPath, imported, imports, _i, _j, _k, _len, _len1, _len2;
-  imported = [];
-  for (_i = 0, _len = allFiles.length; _i < _len; _i++) {
-    file = allFiles[_i];
-    imports = fs.readFileSync(file, 'utf8').match(importRegex);
-    if (imports == null) {
-      continue;
-    }
-    for (_j = 0, _len1 = imports.length; _j < _len1; _j++) {
-      anImport = imports[_j];
-      importRegex.lastIndex = 0;
-      importPath = importRegex.exec(anImport)[1];
-      fullImportPath = path.join(path.dirname(file), importPath);
-      for (_k = 0, _len2 = allFiles.length; _k < _len2; _k++) {
-        fullFilePath = allFiles[_k];
-        console.log("WORKS" + fullFilePath)
-        if (fullFilePath.indexOf(fullImportPath) === 0) {
-          fullImportPath += path.extname(fullFilePath);
-          break;
-        }
-      }
-      imported.push(fullImportPath);
-    }
-  }
-
-  baseFiles = _.difference(allFiles, imported);
-  if (logger.isDebug) {
-    logger.debug("Base files for Stylus are:\n" + (baseFiles.join('\n')));
-  }
-  return baseFiles;
-};
-
-var determineBaseFiless = function (allFiles) {
+var determineBaseFiles = function (allFiles) {
   var imported = []
     , baseFiles;
 
