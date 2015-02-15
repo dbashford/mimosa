5874974b461b53be12faea3252f70cc3657a0adf
david.bashford <dbashford@bericotechnologies.com>
2015-02-15 15:29:30 -0500

    give these a shot on travis

--- test/integration-test.js
+++ test/integration-test.js
@@ -1,9 +1,4 @@
 var path = require( "path" );
-
-process.setMaxListeners(100)
-
-// Can't run these on travis as mimosa is not installed there
-if (__dirname.indexOf("/travis/") < 0) {
-  require("./integrations/util/cleaner-test");
-  require("./integrations/util/watcher-test");
-}
+process.setMaxListeners(100);
+require("./integrations/util/cleaner-test");
+require("./integrations/util/watcher-test");
