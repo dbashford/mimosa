ba234d0cf8baec99f9ebd239d67c675281190a53
david.bashford <dbashford@bericotechnologies.com>
2015-02-20 19:48:18 -0500

    holding place for later configurer tests

---
+++ test/units/util/configurer-test.js
@@ -0,0 +1,13 @@
+// move to configurer tests
+describe("will apply profiles", function() {
+  it("from a .mimosa_profile directory");
+  it("when multiple in a .mimosa_profile directory");
+  it("when added from the command line");
+  it("when multiple added from the command line");
+  describe("in the proper order", function() {
+    it("when multiple profiles in .mimosa_profile");
+    it("when multiple profiles at command line");
+    it("when both .mimosa_profile and command line profile listed");
+    it("when multiple .mimosa_profile and command line profiles are entered");
+  });
+});
