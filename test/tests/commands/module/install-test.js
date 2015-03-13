c2ce61bf57baa466b5496365220070125eb70e08
david.bashford <dbashford@bericotechnologies.com>
2015-03-13 19:58:23 -0400

    more install tests

--- test/tests/commands/module/install-test.js
+++ test/tests/commands/module/install-test.js
@@ -1,5 +1,6 @@
 var cp = require( 'child_process' )
   , path = require( "path" )
+  , wrench = require( "wrench" )
   , fs = require( "fs" )
   , sinon = require( "sinon" )
   , logger = require( "logmimosa" )
@@ -41,7 +42,7 @@ describe("Mimosa's module install command", function() {
             done()
           });
           executeCommand( "mimosa-what");
-        })
+        });
 
         after(function() {
           process.chdir.restore();
@@ -58,26 +59,144 @@ describe("Mimosa's module install command", function() {
       })
 
       describe("if package already exists", function(){
-        it("will back up the package before installing");
-        it("will remove the depedency from the package.json before installing")
+        var writeFileStub
+          , cpdirStub
+          , rmdirStub
+          , library = "copy"
+          ;
 
-        describe("will attempt install", function() {
+        before(function() {
+          sinon.stub(fs, "existsSync", function(){ return true; });
+          writeFileStub = sinon.stub(fs, "writeFileSync", function(){});
+          cpdirStub = sinon.stub(wrench, "copyDirSyncRecursive", function() {});
+          rmdirStub = sinon.stub(wrench, "rmdirSyncRecursive", function() {});
+          sinon.stub(process, "chdir", function() {});
+        });
 
-          it("using the right install string")
+        after(function() {
+          wrench.copyDirSyncRecursive.restore();
+          wrench.rmdirSyncRecursive.restore();
+          fs.writeFileSync.restore();
+          fs.existsSync.restore();
+          process.chdir.restore();
+        });
 
-          describe("if error occurs during install", function(){
-            it("will place backup back");
-            it("will update package.json back to where it was");
-            it("will remove package backup");
-            it("will return to the original directory");
+        describe("basics", function() {
+          var execStub;
+
+          before(function(done) {
+            execStub = sinon.stub( cp, "exec", function(_name) {
+              done();
+            });
+            executeCommand( "mimosa-" + library);
           });
 
-          describe("if install is successful", function(){
-            it("will remove package backup");
-            it("will return to the original directory");
+          after(function() {
+            cp.exec.restore();
           });
 
-        })
+          it("will back up the package before installing", function() {
+            expect(cpdirStub.calledOnce).to.be.true;
+            var beginPath = path.join( process.cwd(), "node_modules", "mimosa-" + library );
+            var endPath = path.join( process.cwd(), "node_modules", "mimosa-" + library + "_____backup" );
+            expect(cpdirStub.args[0][0]).to.eql(beginPath)
+            expect(cpdirStub.args[0][1]).to.eql(endPath)
+          });
+
+          it("will remove the depedency from the package.json before installing", function() {
+            expect(writeFileStub.calledOnce).to.be.true;
+            var packageJSON = JSON.parse(writeFileStub.args[0][1]);
+            expect(Object.keys(packageJSON.dependencies).length).to.be.above(5)
+            expect(packageJSON.dependencies["mimosa-" + library]).to.be.undefined
+          });
+
+          it("will attempt install", function() {
+            expect(execStub.calledOnce).to.be.true;
+            expect(execStub.args[0][0]).to.eql("npm install \"mimosa-copy\" --save")
+          });
+        });
+
+        describe("if error occurs during install", function(){
+
+          before(function(done) {
+            writeFileStub.reset()
+            cpdirStub.reset();
+            rmdirStub.reset()
+            sinon.stub( cp, "exec", function(_name, cb) {
+              cb("ERROR", "", "");
+            });
+            sinon.stub(process, "exit", function() {
+              done();
+            })
+            executeCommand( "mimosa-" + library);
+          });
+
+          after(function() {
+            cp.exec.restore();
+            process.exit.restore();
+          });
+
+          it("will place backup back", function() {
+            expect(cpdirStub.calledTwice).to.be.true;
+            var beginPath = path.join( process.cwd(), "node_modules", "mimosa-" + library );
+            var endPath = path.join( process.cwd(), "node_modules", "mimosa-" + library + "_____backup" );
+            expect(cpdirStub.args[1][0]).to.eql(endPath)
+            expect(cpdirStub.args[1][1]).to.eql(beginPath)
+          });
+
+          it("will update package.json back to where it was", function() {
+            expect(writeFileStub.calledTwice).to.be.true;
+            var packageJSON = JSON.parse(writeFileStub.args[1][1]);
+            expect(Object.keys(packageJSON.dependencies).length).to.be.above(5)
+            expect(typeof packageJSON.dependencies["mimosa-" + library]).to.eql('string');
+          });
+
+          it("will remove package backup", function() {
+            expect(rmdirStub.calledOnce).to.be.true;
+            var endPath = path.join( process.cwd(), "node_modules", "mimosa-" + library + "_____backup" );
+            expect(rmdirStub.args[0][0]).to.eql(endPath);
+          });
+        });
+
+        describe("if install is successful", function(){
+
+          before(function(done) {
+            rmdirStub.reset()
+            sinon.stub( cp, "exec", function(_name, cb) {
+              cb(null, "", "");
+            });
+            sinon.stub(process, "exit", function() {
+              done();
+            })
+            executeCommand( "mimosa-" + library);
+          });
+
+          after(function() {
+            cp.exec.restore();
+            process.exit.restore();
+          });
+
+          it("will remove package backup", function() {
+            expect(rmdirStub.calledOnce).to.be.true;
+            var endPath = path.join( process.cwd(), "node_modules", "mimosa-" + library + "_____backup" );
+            expect(rmdirStub.args[0][0]).to.eql(endPath);
+          });
+
+        });
+
+      });
+
+      describe("will attempt install", function() {
+
+        it("using the right install string")
+
+        describe("if error occurs during install", function(){
+          it("will return to the original directory");
+        });
+
+        describe("if install is successful", function(){
+          it("will return to the original directory");
+        });
       });
     });
   });
