b5184195aba833a9752cb29f6cf97890b00b9ab9
david.bashford <dbashford@bericotechnologies.com>
2015-03-14 17:38:27 -0400

    more tests

--- test/tests/commands/module/uninstall-test.js
+++ test/tests/commands/module/uninstall-test.js
@@ -4,6 +4,8 @@ var cp = require( 'child_process' )
   , fs = require( "fs" )
   , sinon = require( "sinon" )
   , logger = require( "logmimosa" )
+  , modulePath = path.join(process.cwd(), "lib", "modules")
+  , moduleMetadata = require(modulePath).installedMetadata
   , commandPath = path.join( process.cwd(), "lib", "command", "module", "uninstall")
   , command = require( commandPath )
   , utils = require( path.join(process.cwd(), "test", "utils"))
@@ -19,24 +21,26 @@ var executeCommand = function( val ) {
 }
 
 describe("Mimosa's mod:uninstall command", function() {
-  var loggerErrorStub;
+  var loggerErrorStub
+    , chdirStub
+    ;
 
   before(function() {
-    loggerErrorStub = sinon.stub( logger, "error", function(){})
+    loggerErrorStub = sinon.stub( logger, "error", function(){});
+    chdirStub = sinon.stub(process, "chdir", function(){});
+  });
+
+  beforeEach(function() {
+    loggerErrorStub.reset();
   });
 
   after(function() {
     logger.error.restore();
+    process.chdir.restore();
   });
-
 
   describe("when run on local module", function() {
     describe("will error out", function() {
-
-      beforeEach(function() {
-        loggerErrorStub.reset();
-      })
-
       it("when no local package.json", function() {
         sinon.stub(fs, "readFileSync", function() {
           throw new Error("error")
@@ -78,8 +82,16 @@ describe("Mimosa's mod:uninstall command", function() {
       });
 
       it("if module not currently installed in mimosa", function() {
-
+        require(modulePath).installedMetadata = [{name:"mimosa-foo"}];
+        sinon.stub(fs, "readFileSync", function() {
+          return "{\"name\":\"mimosa-require\", \"version\":\"1.1.1\"}";
+        });
+        executeCommand();
+        expect(loggerErrorStub.calledOnce).to.be.true;
+        expect(loggerErrorStub.args[0][0]).to.eql("Module named [[ mimosa-require ]] is not currently installed so it cannot be uninstalled.");
+        require(modulePath).installedMetadata = moduleMetadata;
       });
+
     });
 
     describe("will attempt install", function() {
@@ -98,8 +110,20 @@ describe("Mimosa's mod:uninstall command", function() {
 
   describe("when run on named module", function() {
     describe("will error out", function() {
-      it("when name isn't prefixed with mimosa-")
-      it("if module not currently installed in mimosa")
+      it("when name isn't prefixed with mimosa-", function() {
+        it("when name isn't prefixed with mimosa-", function() {
+          executeCommand("foo");
+          expect(loggerErrorStub.calledOnce).to.be.true;
+          expect(loggerErrorStub.args[0][0]).to.eql("Can only delete 'mimosa-' prefixed modules with mod:uninstall (ex: mimosa-server).");
+        });
+      })
+      it("if module not currently installed in mimosa", function() {
+        require(modulePath).installedMetadata = [{name:"mimosa-foo"}];
+        executeCommand("mimosa-bar");
+        expect(loggerErrorStub.calledOnce).to.be.true;
+        expect(loggerErrorStub.args[0][0]).to.eql("Module named [[ mimosa-bar ]] is not currently installed so it cannot be uninstalled.");
+        require(modulePath).installedMetadata = moduleMetadata;
+      })
     });
 
     describe("will attempt install", function() {
